252769f68de2800f25590c2b4674995b6a40a387
chenzb@gzfintech.com <chenzb@gzfintech.com>
2026-04-01 16:34:50 +0800

    modify __init__

--- data_server/models/__init__.py
+++ data_server/models/__init__.py
@@ -7,8 +7,6 @@ from .fund_industry_allocation import FundIndustryAllocation
 from .fund_portfolio_hold import FundPortfolioHold
 from .fund_index_mapping import FundIndexMapping
 from .index_weights import IndexWeight
-from .portfolio_item import PortfolioItem
-from .portfolio_nav_history import PortfolioNavHistory
 from .fund_asset_allocation import FundAssetAllocation
 from .sw_index_info import SwIndexInfo
 from .fund_valuation_audit import FundValuationAudit
