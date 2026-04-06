1f3e79dbe91d27f40e1af2c3c81b68cd547c2100
czbsunny <czbsunny@gmail.com>
2026-04-06 16:45:14 +0800

    add interface

--- server/src/main/java/com/suiyou/controller/AssetSnapshotController.java
+++ server/src/main/java/com/suiyou/controller/AssetSnapshotController.java
@@ -10,8 +10,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
